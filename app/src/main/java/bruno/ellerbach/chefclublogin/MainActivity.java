8f59528e3a978b0d099cd3d0d8b8fb7b91917256
Bruno Fernandez <bruno.sistemas@outlook.com>
2019-08-03 21:17:06 -0300

    [Feature]Implemented toasts to REGISTER and FORGOT PASSWORD texts.

--- app/src/main/java/bruno/ellerbach/chefclublogin/MainActivity.java
+++ app/src/main/java/bruno/ellerbach/chefclublogin/MainActivity.java
@@ -3,12 +3,38 @@ package bruno.ellerbach.chefclublogin;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText editTextEmail, editTextPassword;
+    TextView textViewForgotPassword, textViewRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editTextEmail = findViewById(R.id.editTextEmail);
+        editTextPassword = findViewById(R.id.editTextPassword);
+        textViewForgotPassword = findViewById(R.id.textViewForgotPassword);
+        textViewRegister = findViewById(R.id.textViewRegister);
+
+        textViewForgotPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Forgot Password button clicked", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        textViewRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Register button clicked", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
